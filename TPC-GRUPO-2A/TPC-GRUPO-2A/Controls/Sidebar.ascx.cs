8050f4eef995e564e53a22a33884923c7d418915
juan <calvojuancito@gmail.com>
2026-05-31 00:29:51 -0300

    feat: se armo mockup de front , este contiene un menu sidebar, y un header para lo que seria buqueda y usuario logueado

---
+++ TPC-GRUPO-2A/TPC-GRUPO-2A/Controls/Sidebar.ascx.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace TPC_GRUPO_2A.Controls
+{
+    public partial class Sidebar : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MarcarLinkActivo();
+        }
+
+        private void MarcarLinkActivo()
+        {
+            string paginaActual = Request.AppRelativeCurrentExecutionFilePath.ToLower();
+
+            QuitarActivo(lnkDashboard, lnkReclamos, lnkClientes, lnkUsuarios, lnkTipos, lnkPrioridades);
+
+            if (paginaActual.Contains("reclamos"))
+                Activar(lnkReclamos);
+            else if (paginaActual.Contains("clientes"))
+                Activar(lnkClientes);
+            else if (paginaActual.Contains("usuarios"))
+                Activar(lnkUsuarios);
+            else if (paginaActual.Contains("tiposincidencias"))
+                Activar(lnkTipos);
+            else if (paginaActual.Contains("prioridades"))
+                Activar(lnkPrioridades);
+            else
+                Activar(lnkDashboard);
+        }
+
+        private void QuitarActivo(params HtmlAnchor[] links)
+        {
+            foreach (HtmlAnchor link in links)
+                link.Attributes["class"] = "sidebar-link";
+        }
+
+        private void Activar(HtmlAnchor link)
+        {
+            link.Attributes["class"] = "sidebar-link active";
+        }
+    }
+}
