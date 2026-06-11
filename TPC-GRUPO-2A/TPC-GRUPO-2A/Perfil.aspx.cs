059c641233c99ae877f152734e6374f5fc56502a
LucioNiderhaus <lucholuque98@gmail.com>
2026-06-11 09:17:06 -0300

    Creación Perfil.aspx / Corrección script de DB con Apellido

--- TPC-GRUPO-2A/TPC-GRUPO-2A/Perfil.aspx.cs
+++ TPC-GRUPO-2A/TPC-GRUPO-2A/Perfil.aspx.cs
@@ -1,5 +1,4 @@
-﻿using negocio;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace TPC_GRUPO_2A
 {
-    public partial class Usuarios : Page
+    public partial class Perfil : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            UsuarioNegocio negocio = new UsuarioNegocio();
-            dgvUsuarios.DataSource = negocio.ObtenerTodos();
-            dgvUsuarios.DataBind();
+
         }
     }
 }
