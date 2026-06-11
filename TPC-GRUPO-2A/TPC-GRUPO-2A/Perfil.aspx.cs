245a71b798d67f39888a376b53f52ff20a32278f
LucioNiderhaus <lucholuque98@gmail.com>
2026-06-11 09:46:35 -0300

    Cargar datos en perfil

--- TPC-GRUPO-2A/TPC-GRUPO-2A/Perfil.aspx.cs
+++ TPC-GRUPO-2A/TPC-GRUPO-2A/Perfil.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,24 @@ namespace TPC_GRUPO_2A
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            CargarDatos();
+        }
 
+        private void CargarDatos()
+        {
+            try
+            {
+                Usuario u = (Usuario)Session["usuario"];
+                txtNombre.Text = u.Nombre;
+                txtApellido.Text = u.Apellido;
+                txtEmail.Text = u.Email;
+                txtRol.Text = u.Rol.Nombre;
+                txtFechaCreacion.Text = u.FechaCreacion.ToString("dd/MM/yyyy");
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex);
+            }
         }
     }
 }
