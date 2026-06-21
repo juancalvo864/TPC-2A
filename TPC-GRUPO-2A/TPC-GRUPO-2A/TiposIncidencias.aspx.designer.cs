fc2ce9df0f75a5bc80a780d87799a58ab5b211e9
LucioNiderhaus <lucholuque98@gmail.com>
2026-06-20 21:31:00 -0300

    Diseño TiposIncidencias

--- TPC-GRUPO-2A/TPC-GRUPO-2A/TiposIncidencias.aspx.designer.cs
+++ TPC-GRUPO-2A/TPC-GRUPO-2A/TiposIncidencias.aspx.designer.cs
@@ -24,6 +24,42 @@ namespace TPC_GRUPO_2A
         protected global::System.Web.UI.WebControls.Button btnNuevo;
 
         /// <summary>
+        /// Control ddlEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEstado;
+
+        /// <summary>
+        /// Control txtBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBuscar;
+
+        /// <summary>
+        /// Control btnFiltrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnFiltrar;
+
+        /// <summary>
+        /// Control btnLimpiar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLimpiar;
+
+        /// <summary>
         /// Control pnlFormulario.
         /// </summary>
         /// <remarks>
