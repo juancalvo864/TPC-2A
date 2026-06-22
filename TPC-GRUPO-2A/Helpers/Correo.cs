53216a30154e7a86ca349df5c82e65db021d29c1
LucioNiderhaus <lucholuque98@gmail.com>
2026-06-22 11:32:35 -0300

    Proyecto Helpers junto con clase correo

---
+++ TPC-GRUPO-2A/Helpers/Correo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class Correo
+    {
+        public void EnviarCorreo(string destinatario, string asunto, string cuerpo)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("armetal.autopartes@gmail.com", "CallCenter");
+                mail.To.Add(destinatario);
+                mail.Subject = asunto;
+                mail.Body = cuerpo;
+                mail.IsBodyHtml = false;
+
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new NetworkCredential("armetal.autopartes@gmail.com", "pqtsxlnvbkgfsgln");
+                smtpServer.EnableSsl = true;
+                smtpServer.Send(mail);
+            }
+            catch (SmtpException smtpEx)
+            {
+                throw new Exception("Error SMTP: " + smtpEx.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al enviar el correo: " + ex.Message);
+            }
+        }
+    }
+}
